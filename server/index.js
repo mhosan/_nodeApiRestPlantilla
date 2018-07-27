47876af5be814ec084ee204abf5f2c81ff4c6eba
mhosan <mhosan@gmail.com>
2018-07-27 13:21:49 -0300

    el segundo del dia

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');         //web server
-const morgan = require('morgan');           //para ver mensajes en el servidor
-const cors = require('cors');               //proxy
+const express = require('express');                 //web server
+const morgan = require('morgan');                   //para ver mensajes en el servidor
+const cors = require('cors');                       //proxy
 const app = express();
-const { mongoose } = require('./database'); //driver para mongodb, ahi se establece la conexión al mongo (cerrar?)
+const { mongoose } = require('./database');         //driver para mongodb, ahi se establece la conexión al mongo (cerrar?) <------------------
 
-app.set('port', process.env.PORT || 3000);  //usar el puerto que me asigne el serv hosting
-                                            //si el puerto no existe, utilizar el 3000
+app.set('port', process.env.PORT || 3000);          //usar el puerto que me asigne el serv hosting
+                                                    //si el puerto no existe, utilizar el 3000
 
 //midlewares
-app.use(morgan('dev'));     //ver los mensajes en el servidor
-app.use(express.json());    //que el servidor entienda json
-//app.use(cors({origin: 'http://localhost:4200'}));
-app.use(cors({}));
+app.use(morgan('dev'));                             //ver los mensajes en el servidor
+app.use(express.json());                            //que el servidor entienda json
+//app.use(cors({origin: 'http://localhost:4200'})); //que el servidor escuche solamente en el puerto 4200
+app.use(cors({}));                                  //que el servidor escuche en todos los puertos
 
 //routes
-app.use('/',require('./routes/rutas.js'));  //el primer parametro es la ruta
-                                            //por default. Se agrega a la ruta '/'
+app.use('/apimh',require('./routes/rutas.js'));       //el primer parametro es la ruta
+                                                    //por default. Se agrega a la ruta '/'
 
 //start el server
 app.listen(app.get('port'), () => {
